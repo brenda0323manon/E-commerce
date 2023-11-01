812ea38471db56182dfb69665f1761f2d10e7b03
brendamanon0526@gmail <Brenda_Manon@adminisatorsmbp.attlocal.net>
2023-10-31 22:10:50 -0400

    add the contact

---
+++ Contact/contact.js
@@ -0,0 +1,18 @@
+const contact = document.getElementsByClassName('contact-us');
+const email = document.getElementsByClassName('Email');
+const emailFooter =document.getElementsByClassName('email');
+const name = document.getElementsByClassName('name');
+
+FormData.addEventListener('signUp', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+
+});
+
+function checkInputs() {
+    contact.value
+    email.value
+    email.value
+    name.value
+}
