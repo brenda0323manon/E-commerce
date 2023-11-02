522731d267a19520832e8b7bf9191bfebf227582
brenda0323manon <brendamanon0526@gmail.com>
2023-11-02 12:04:32 -0400

    edit

--- Contact/contact.js
+++ Contact/contact.js
@@ -21,8 +21,8 @@
   }
 
   function validateName(name) {
-    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailRegex.test(email);
+    const nameRegex = /^[A-Za-z]+(?:\s[A-Za-z]+)*$/;
+    return nameRegex.test(name);
   }
 
   
